fe3734090f32fc34dd04a73a919d912aa508f528
Nikarko01 <barchinicola@yahoo.com>
2018-04-19 18:15:31 +0200

    save new ibrav2 7a data

--- Lab_2/p7/script_deneb.sh
+++ Lab_2/p7/script_deneb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes 1
 #SBATCH --ntasks 8
-#SBATCH --time=00:30:00
+#SBATCH --time=00:20:00
 
 #These
 module purge
@@ -12,7 +12,7 @@ module load fftw/3.3.6-pl2
 module load espresso/6.1.0-mpi
 
 
-LISTA="5.5 6 6.5 6.6 6.7 6.8 6.9 7.0 7.1 7.2 7.3 7.4" # List of values of lattice parameter to try
+LISTA="7.887 7.896 7.904 7.913 7.921 7.929 7.938 7.946 7.955 7.963 7.971 7.980 7.988 7.996 8.005 8.013 8.022 8.030 8.038 8.047" # List of values of lattice parameter to try
 LISTECUT="70"          # List of plane-wave cutoffs to try
 LISTK="4"              # List of number of k-points per dimension to try.
 
